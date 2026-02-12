7f24593416252558a295a2260acb6964896bfdfa
GregDawson03 <79373275+greg1125@users.noreply.github.com>
2026-02-12 08:34:28 -0800

    Create wakepc.sh

---
+++ wakepc.sh
@@ -0,0 +1,23 @@
+## wakepc.sh, script used to turn on desktop remotely 
+
+```bash
+#!/bin/bash
+
+echo "Sending Wake-on-LAN packet..."
+wakeonlan AA:BB:CC:DD:EE:FF
+
+echo "Waiting 20 seconds for system boot..."
+sleep 20
+
+echo "System should now be reachable via RDP."
+
+#############################################
+
+chmod +x wakepc.sh
+
+#########################################
+
+
+alias wakepc="~/wakepc.sh"
+
+
